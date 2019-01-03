d8ddc5df5a946c45046908386483c19ee365cfb9
Chad Lee <chad.lee@civicsource.com>
2019-01-03 11:49:04 -0600

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,109 +27,119 @@ if (!spaceId || !accessToken) {
 	);
 }
 
-module.exports = {
-	siteMetadata: {
-		title: "chadly.net",
-		description: "Personal blog by Chad Lee",
-		author: {
-			name: "Chad Lee",
-			blurb:
-				"Technical Lead at CivicSource.com, OSS developer, beginner dad, novice human.",
-			social: {
-				github: "chadly",
-				twitter: "wchadly",
-				keybase: "chadly"
-			}
-		},
-		siteUrl: "https://www.chadly.net",
-		disqus: process.env.DISQUS_SHORTNAME || ""
+const siteMetadata = {
+	title: "chadly.net",
+	description: "Personal blog by Chad Lee",
+	author: {
+		name: "Chad Lee",
+		blurb:
+			"Technical Lead at CivicSource.com, OSS developer, beginner dad, novice human.",
+		social: {
+			github: "chadly",
+			twitter: "wchadly",
+			keybase: "chadly"
+		}
 	},
-	plugins: [
-		"gatsby-plugin-sharp",
-		{
-			resolve: "gatsby-transformer-remark",
-			options: {
-				plugins: [
-					"gatsby-remark-autolink-headers",
-					"gatsby-remark-prismjs",
-					"gatsby-remark-reading-time"
-				]
-			}
-		},
-		"gatsby-plugin-catch-links",
-		"gatsby-plugin-react-helmet",
-		{
-			resolve: "gatsby-plugin-typography",
-			options: {
-				pathToConfigModule: "src/typography"
-			}
-		},
-		{
-			resolve: "gatsby-source-contentful",
-			options: contentfulConfig
-		},
-		"gatsby-plugin-jss",
-		"gatsby-plugin-sitemap",
-		"gatsby-plugin-robots-txt",
-		{
-			resolve: `gatsby-plugin-feed`,
-			options: {
-				query: `
-				{
-					site {
-						siteMetadata {
-							title
-							description
-							siteUrl
-							site_url: siteUrl
-						}
-					}
-				}`,
-				feeds: [
-					{
-						serialize: ({ query: { site, allContentfulBlogPost } }) => {
-							return allContentfulBlogPost.edges.map(edge => {
-								const url = calculateCanonicalUrl({
-									siteUrl: site.siteMetadata.siteUrl,
-									slug: edge.node.slug
-								});
+	siteUrl: "https://www.chadly.net",
+	disqus: process.env.DISQUS_SHORTNAME || ""
+};
 
-								return Object.assign({}, edge.node, {
-									description: edge.node.body.childMarkdownRemark.excerpt,
-									date: edge.node.publishDate,
-									url,
-									guid: url,
-									custom_elements: [
-										{
-											"content:encoded": edge.node.body.childMarkdownRemark.html
-										}
-									]
-								});
+const plugins = [
+	"gatsby-plugin-sharp",
+	{
+		resolve: "gatsby-transformer-remark",
+		options: {
+			plugins: [
+				"gatsby-remark-autolink-headers",
+				"gatsby-remark-prismjs",
+				"gatsby-remark-reading-time"
+			]
+		}
+	},
+	"gatsby-plugin-catch-links",
+	"gatsby-plugin-react-helmet",
+	{
+		resolve: "gatsby-plugin-typography",
+		options: {
+			pathToConfigModule: "src/typography"
+		}
+	},
+	{
+		resolve: "gatsby-source-contentful",
+		options: contentfulConfig
+	},
+	"gatsby-plugin-jss",
+	"gatsby-plugin-sitemap",
+	"gatsby-plugin-robots-txt",
+	{
+		resolve: `gatsby-plugin-feed`,
+		options: {
+			query: `
+			{
+				site {
+					siteMetadata {
+						title
+						description
+						siteUrl
+						site_url: siteUrl
+					}
+				}
+			}`,
+			feeds: [
+				{
+					serialize: ({ query: { site, allContentfulBlogPost } }) => {
+						return allContentfulBlogPost.edges.map(edge => {
+							const url = calculateCanonicalUrl({
+								siteUrl: site.siteMetadata.siteUrl,
+								slug: edge.node.slug
 							});
-						},
-						query: `
-						{
-							allContentfulBlogPost(limit: 1000, sort: { fields: [publishDate], order: DESC }) {
-								edges {
-									node {
-										title
-										slug
-										publishDate(formatString: "YYYY-MM-DD")
-										body {
-											childMarkdownRemark {
-												excerpt
-												html
-											}
+
+							return Object.assign({}, edge.node, {
+								description: edge.node.body.childMarkdownRemark.excerpt,
+								date: edge.node.publishDate,
+								url,
+								guid: url,
+								custom_elements: [
+									{
+										"content:encoded": edge.node.body.childMarkdownRemark.html
+									}
+								]
+							});
+						});
+					},
+					query: `
+					{
+						allContentfulBlogPost(limit: 1000, sort: { fields: [publishDate], order: DESC }) {
+							edges {
+								node {
+									title
+									slug
+									publishDate(formatString: "YYYY-MM-DD")
+									body {
+										childMarkdownRemark {
+											excerpt
+											html
 										}
 									}
 								}
 							}
-						}`,
-						output: "/rss.xml",
-						title: "Gatsby RSS Feed"
-					}
-				]
-			}
+						}
+					}`,
+					output: "/rss.xml",
+					title: "Gatsby RSS Feed"
+				}
+			]
 		}
-	]
-};
+	}
+];
+
+if (process.env.GOOGLE_ANALYTICS_TRACKING_ID) {
+	plugins.push({
+		resolve: "gatsby-plugin-google-analytics",
+		options: {
+			trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID
+		}
+	});
+}
+
+module.exports = { siteMetadata, plugins };
