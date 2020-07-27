1b67efedb60d1b121f02e3258490afe1c86e949c
Chad Lee <git@chadly.net>
2020-07-27 10:47:30 -0500

    Add twitter link

--- src/post/feedback/twitter.js
+++ src/post/feedback/twitter.js
@@ -8,46 +8,58 @@ const TwitterIntents = ({ twitterId }) => {
 	if (!twitterId) return null;
 
 	return (
-		<ul className={classes.root}>
-			<li>
-				<a
-					href={`https://twitter.com/intent/tweet?in_reply_to=${twitterId}`}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<i className="fas fa-reply" /> Reply
-				</a>
-			</li>
-			<li>
-				<a
-					href={`https://twitter.com/intent/retweet?tweet_id=${twitterId}`}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<i className="fas fa-retweet" /> Repost
-				</a>
-			</li>
-			<li>
-				<a
-					href={`https://twitter.com/intent/favorite?tweet_id=${twitterId}`}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<i className="fas fa-heart" /> Like
-				</a>
-			</li>
-		</ul>
+		<div className={classes.root}>
+			<ul className={classes.list}>
+				<li>
+					<a
+						href={`https://twitter.com/intent/tweet?in_reply_to=${twitterId}`}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<i className="fas fa-reply" /> Reply
+					</a>
+				</li>
+				<li>
+					<a
+						href={`https://twitter.com/intent/retweet?tweet_id=${twitterId}`}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<i className="fas fa-retweet" /> Repost
+					</a>
+				</li>
+				<li>
+					<a
+						href={`https://twitter.com/intent/favorite?tweet_id=${twitterId}`}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<i className="fas fa-heart" /> Like
+					</a>
+				</li>
+			</ul>
+
+			<span className={classes.desc}>on Twitter</span>
+		</div>
 	);
 };
 
 const useStyles = createUseStyles({
 	root: {
-		textAlign: "right",
+		display: "flex",
+		justifyContent: "flex-end",
+		alignItems: "baseline"
+	},
+	list: {
 		"& li": {
 			display: "inline-block",
 			margin: `0 ${rhythm(0.2)}`,
 			...scale(-0.2)
 		}
+	},
+	desc: {
+		color: "var(--textMuted)",
+		fontSize: "0.75rem"
 	}
 });
 
