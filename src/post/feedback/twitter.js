aa5295c8a57edccc85ae31c4f4d471e0f6e457b0
Chad Lee <git@chadly.net>
2020-07-27 14:15:07 -0500

    Use twitter icon

--- src/post/feedback/twitter.js
+++ src/post/feedback/twitter.js
@@ -6,6 +6,7 @@ import SvgIcon from "../../icon";
 import ReplyIcon from "./reply.svg";
 import RetweetIcon from "./retweet.svg";
 import HeartIcon from "./heart.svg";
+import TwitterIcon from "../../author/twitter.svg";
 
 const TwitterIntents = ({ twitterId }) => {
 	const classes = useStyles();
@@ -13,63 +14,64 @@ const TwitterIntents = ({ twitterId }) => {
 	if (!twitterId) return null;
 
 	return (
-		<div className={classes.root}>
-			<ul className={classes.list}>
-				<li>
-					<a
-						href={`https://twitter.com/intent/tweet?in_reply_to=${twitterId}`}
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<SvgIcon>
-							<ReplyIcon />
-						</SvgIcon>{" "}
-						Reply
-					</a>
-				</li>
-				<li>
-					<a
-						href={`https://twitter.com/intent/retweet?tweet_id=${twitterId}`}
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<SvgIcon>
-							<RetweetIcon />
-						</SvgIcon>{" "}
-						Repost
-					</a>
-				</li>
-				<li>
-					<a
-						href={`https://twitter.com/intent/favorite?tweet_id=${twitterId}`}
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<SvgIcon>
-							<HeartIcon />
-						</SvgIcon>{" "}
-						Like
-					</a>
-				</li>
-			</ul>
-
-			<span className={classes.desc}>on Twitter</span>
-		</div>
+		<ul className={classes.list}>
+			<li className={classes.twitter} title="Twitter">
+				<SvgIcon>
+					<TwitterIcon />
+				</SvgIcon>
+			</li>
+			<li>
+				<a
+					href={`https://twitter.com/intent/tweet?in_reply_to=${twitterId}`}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<SvgIcon>
+						<ReplyIcon />
+					</SvgIcon>{" "}
+					Reply
+				</a>
+			</li>
+			<li>
+				<a
+					href={`https://twitter.com/intent/retweet?tweet_id=${twitterId}`}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<SvgIcon>
+						<RetweetIcon />
+					</SvgIcon>{" "}
+					Repost
+				</a>
+			</li>
+			<li>
+				<a
+					href={`https://twitter.com/intent/favorite?tweet_id=${twitterId}`}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<SvgIcon>
+						<HeartIcon />
+					</SvgIcon>{" "}
+					Like
+				</a>
+			</li>
+		</ul>
 	);
 };
 
 const useStyles = createUseStyles({
-	root: {
-		display: "flex",
-		justifyContent: "flex-end",
-		alignItems: "baseline"
-	},
 	list: {
+		textAlign: "right",
+
 		"& li": {
 			display: "inline-block",
 			margin: `0 ${rhythm(0.2)}`,
 			...scale(-0.3)
 		}
+	},
+	twitter: {
+		color: "#1c9ceb"
 	},
 	desc: {
 		color: "var(--textMuted)",
