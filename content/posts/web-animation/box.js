b3057ae009e81ccbca53df9983e2e0c87b667762
Chad Lee <git@chadly.net>
2020-08-09 16:03:59 -0500

    Flexbox some elements on web animation post

--- content/posts/web-animation/box.js
+++ content/posts/web-animation/box.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createUseStyles } from "react-jss";
-import { rhythm } from "../../../src/theme";
+import { rhythm, smallScreenMediaQuery } from "../../../src/theme";
 
 export const Box = ({ children }) => {
 	const classes = useStyles();
@@ -12,6 +12,11 @@ export const BackgroundBox = ({ children }) => {
 	return <div className={classes.rootWithBg}>{children}</div>;
 };
 
+export const FlexBox = ({ children }) => {
+	const classes = useStyles();
+	return <div className={classes.flex}>{children}</div>;
+};
+
 const useStyles = createUseStyles({
 	root: {
 		display: "flex",
@@ -21,13 +26,34 @@ const useStyles = createUseStyles({
 		margin: `${rhythm(2)} 0`,
 
 		"& .lottie": {
-			width: "50%"
+			width: "50%",
+
+			[smallScreenMediaQuery]: {
+				width: "100%"
+			}
 		}
 	},
 	rootWithBg: {
 		composes: "$root",
 		"& .lottie svg": {
 			backgroundColor: "#000"
+		}
+	},
+	flex: {
+		composes: "$root",
+		display: "flex",
+		flexWrap: "wrap",
+		alignItems: "center",
+		justifyContent: "space-between",
+
+		"& p": {
+			width: "50%"
+		},
+
+		[smallScreenMediaQuery]: {
+			"& p": {
+				width: "100%"
+			}
 		}
 	}
 });
